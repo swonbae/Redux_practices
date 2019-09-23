fbd50ccfa8fbc92b764c1bfb12e91495e12381a2
swonb <swonbinca@gmail.com>
2019-09-22 20:00:05 -0400

    reducer state change test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+// import reducer from './reducers'
+import { createStore } from "redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const reducer = function(state, action) {
+  if (action.type === "INC") {
+    return state + action.payload;
+  }
+  if (action.type === "DEC") {
+    return state - action.payload;
+  }
+  return state;
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const store = createStore(reducer, 0);
+
+store.subscribe(() => {
+  console.log("store changed", store.getState());
+});
+
+store.dispatch({ type: "INC", payload: 1 });
+store.dispatch({ type: "INC", payload: 2 });
+store.dispatch({ type: "INC", payload: 22 });
+store.dispatch({ type: "DEC", payload: 1 });
+
+ReactDOM.render(<App />, document.getElementById("root"));
