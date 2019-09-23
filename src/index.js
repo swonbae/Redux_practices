6131f8aee58bbada7f11ba9767c542249a237541
swonb <swonbinca@gmail.com>
2019-09-22 20:14:08 -0400

    combineReducers state test

--- src/index.js
+++ src/index.js
@@ -3,27 +3,49 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 // import reducer from './reducers'
-import { createStore } from "redux";
+import { combineReducers, createStore } from "redux";
 
-const reducer = function(state, action) {
-  if (action.type === "INC") {
-    return state + action.payload;
-  }
-  if (action.type === "DEC") {
-    return state - action.payload;
+// const reducer = function(state, action) {
+//   if (action.type === "INC") {
+//     return state + action.payload;
+//   }
+//   if (action.type === "DEC") {
+//     return state - action.payload;
+//   }
+//   return state;
+// };
+
+const userReducer = (state = {}, action) => {
+  switch (action.type) {
+    case "CHANGE_NAME": {
+      state = { ...state, name: action.payload };
+      break;
+    }
+    case "CHANGE_AGE": {
+      state = { ...state, age: action.payload };
+      break;
+    }
   }
   return state;
 };
 
-const store = createStore(reducer, 0);
+const tweetsReducer = (state = [], action) => {
+  return state;
+};
+
+const reducers = combineReducers({
+  user: userReducer,
+  tweets: tweetsReducer
+});
+
+const store = createStore(reducers);
 
 store.subscribe(() => {
   console.log("store changed", store.getState());
 });
 
-store.dispatch({ type: "INC", payload: 1 });
-store.dispatch({ type: "INC", payload: 2 });
-store.dispatch({ type: "INC", payload: 22 });
-store.dispatch({ type: "DEC", payload: 1 });
+store.dispatch({ type: "CHANGE_NAME", payload: "Will" });
+store.dispatch({ type: "CHANGE_AGE", payload: 35 });
+store.dispatch({ type: "CHANGE_AGE", payload: 36 });
 
 ReactDOM.render(<App />, document.getElementById("root"));
